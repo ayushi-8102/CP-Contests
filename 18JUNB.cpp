de9675a1a53e96b0e70abbf6488d3a4eed874c7e
Ayushi Shukla <91957156+ayushi-8102@users.noreply.github.com>
2022-12-16 06:45:54 +0530

    ;)

---
+++ 18JUNB.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){;
+int t,n,p;
+  cin>>t;
+  while(t--){
+  cin>>n;
+    while(n--){
+      cin>>p;
+    }
+if(p%2==0)cout<<"Joe";
+    else cout<<"Mike";
+  }
+}
